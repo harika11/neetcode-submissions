f9d0b31d4bb3ec61cc5255b3650911778ef18f67
Duvvvuri Laxmi Harika <laxmiharika33@gmail.com>
2026-07-07 22:21:38 -0400

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int ans = 0;
+
+        foreach (int num in nums) {
+            ans ^= num;
+        }
+
+        return ans;
+    }
+}
