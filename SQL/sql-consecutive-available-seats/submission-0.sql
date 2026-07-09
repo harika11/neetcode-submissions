192c4c5335f3a2d7dfdf6e628ccabf281ef31a5e
Duvvvuri Laxmi Harika <laxmiharika33@gmail.com>
2026-07-08 22:56:58 -0400

    Add: sql-consecutive-available-seats - submission-0

---
+++ SQL/sql-consecutive-available-seats/submission-0.sql
@@ -0,0 +1,14 @@
+-- Write your query below
+SELECT c1.seat_id
+FROM cinema c1
+LEFT JOIN cinema c2
+    ON c1.seat_id = c2.seat_id - 1
+LEFT JOIN cinema c3
+    ON c1.seat_id = c3.seat_id + 1
+WHERE c1.free = 1
+  AND (
+        c2.free = 1
+        OR
+        c3.free = 1
+      )
+ORDER BY c1.seat_id;
